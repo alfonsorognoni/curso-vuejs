f8660bb371b1ed018cfa7959b702e4ac7d8851ed
Alfonso Rognoni <alfonsorognoni@gmail.com>
2018-07-08 23:31:08 -0300

    search

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,53 @@
   #app
     section.section
       nav.nav.has-shadow
-        .container.field.has-addons
-          input.input.is-large(type="text", placeholder="Buscar Canciones")
-          a.button.is-info.is-large Buscar
-          a.button.is-danger.is-large &times;
+        .container
+          .field.has-addons
+            input.input.is-large(
+              type="text",
+              placeholder="Buscar Canciones",
+              v-model="searchQuery"
+            )
+            a.button.is-info.is-large(@click="search") Buscar
+            a.button.is-danger.is-large &times;
+          p
+            small {{ searchMessage }}
       //results
-      .container
+      .container.results
         .columns
-          .column
+          .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
 </template>
 
 <script>
+const tracks = [
+  { name: 'Muchacha ojos de papel', artist: 'Almendra' },
+  { name: '11 y 6', artist: 'Fito Paez' },
+  { name: 'Puente', artist: 'Gustavo Cerati' }
+]
 export default {
   name: 'app',
   data () {
     return {
-
+      searchQuery: '',
+      tracks: []
+    }
+  },
+  computed: {
+    searchMessage () {
+      return `Encontrados ${this.tracks.length}`
     }
   },
   methods: {
-
+    search () {
+      this.tracks = tracks
+    }
   }
 }
 </script>
 
 <style lang="scss">
   @import './scss/main.scss';
+  .results {
+    margin-top: 50px;
+  }
 </style>
