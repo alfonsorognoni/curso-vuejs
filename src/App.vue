e8c433b27d68814653279630cc1cb432b52f8697
Alfonso Rognoni <alfonsorognoni@gmail.com>
2018-07-09 23:58:32 -0300

    ejercicio repaso "tasks"

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,33 @@
       .container.results
         .columns
           .column(v-for="t in tracks") {{ t.name }} - {{ t.artist }}
+      .container.results
+        .columns
+          .column
+
+            .field.is-grouped
+              p.control.is-expanded
+                input.input(
+                  v-model="newTask.title",
+                  type="text",
+                  placeholder="Task Title"
+                )
+              p.control.is-expanded
+                input.input(
+                  v-model="newTask.time",
+                  type="text",
+                  placeholder="Task Time"
+                )
+              p.control
+                a.button.is-info(@click="addTask") Add
+              p.control
+                a.button.is-danger(@click="cancelTask") Cancel
+
+            p {{ name }}
+            ul
+              li(v-for="(task, index) in tasks") {{ task.title }} - {{ task.time }}
+                a.button.is-danger(@click="removeTask(index)") Remove Task
+            p Total time: {{ totalTime }}
 </template>
 
 <script>
@@ -25,22 +52,59 @@ const tracks = [
   { name: '11 y 6', artist: 'Fito Paez' },
   { name: 'Puente', artist: 'Gustavo Cerati' }
 ]
+/*
+const tasks = [
+  { title: 'Tarea 1', time: 1 }
+]
+*/
 export default {
   name: 'app',
   data () {
     return {
       searchQuery: '',
-      tracks: []
+      tracks: [],
+      name: 'Alfonso',
+      tasks: [],
+      newTask: { title: '', time: 0 }
+    }
+  },
+  created: function () {
+    // `this` points to the vm instance
+    if (localStorage.getItem('tasks')) {
+      this.tasks = JSON.parse(localStorage.getItem('tasks'))
     }
   },
   computed: {
     searchMessage () {
       return `Encontrados ${this.tracks.length}`
+    },
+    totalTime () {
+      let total = 0
+      for (let index = 0; index < this.tasks.length; index++) {
+        total += parseFloat(this.tasks[index].time)
+      }
+      return total
     }
   },
   methods: {
     search () {
       this.tracks = tracks
+    },
+    addTask () {
+      if (this.newTask.title !== '' && this.newTask.time > 0) {
+        let task = this.newTask
+        this.tasks.push(task)
+        this.newTask = { title: '', time: 0 }
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      }
+    },
+    cancelTask () {
+      this.newTask.title = ''
+      this.newTask.time = 0
+    },
+    removeTask (index) {
+      this.tasks.splice(index, 1)
+      localStorage.setItem('tasks', JSON.stringify(this.tasks))
     }
   }
 }
